f24b9657f5fecd6025df52b1a6bc5e71ed8b8086
Syed Ali <syed01062006@gmail.com>
2025-09-16 21:06:11 +0500

    Create 04-reverse-array.cpp
    Add reverse array program

---
+++ DSA/Arrays/04-reverse-array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int* arr = new int[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Reverse in place
+    for (int i = 0, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    arr = nullptr;
+
+    return 0;
+}
